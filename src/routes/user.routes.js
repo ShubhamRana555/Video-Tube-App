d0dadca58f9b2c85f5f3f2690010d42576396c80
Shubham <shubhamrana0108@gmail.com>
2024-09-19 19:24:21 +0530

    add user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from '../controllers/user.controller.js'; 
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from '../controllers/user.controller.js'; 
 import {upload} from '../middlewares/multer.middleware.js';
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verify } from "jsonwebtoken";
 
 
 const router = Router();
@@ -17,7 +18,7 @@ router.route("/register").post(
             maxCount: 1
         }
     ]), // injecting middleware just before 'registerUser' controller
-    regist  erUser
+    registerUser
 )
 
 router.route("/login").post(loginUser)
@@ -25,7 +26,16 @@ router.route("/login").post(loginUser)
 // secured routes
 router.route("/logout").post(verifyJWT ,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails) // using patch method to update the account details specifically
 
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+
+// we got {username} from params, so we have to name url exactly like we defined in params in controller
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory )
 
 
 
